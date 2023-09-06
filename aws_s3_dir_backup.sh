5f948f5300e69cfac803b37c4f282e52f3115f87
Maciej Sawiński <macsawinski@gmail.com>
2023-09-06 18:50:18 +0200

    added aws_s3 dir backup script

---
+++ aws_s3_dir_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# variables
+S3_BUCKET=""
+DIR=""
+EXCLUDE_LIST=(
+        "node_modules"
+        "build"
+        ".cache"
+        ".git"
+)
+
+# create gzip file
+FILE_NAME="${DIR}_$(date +%Y%m%d%H%M).tar.gz"
+tar --exclude-from=<(printf "%s\n" "${EXCLUDE_LIST[@]}") -czf $FILE_NAME $DIR
+
+# upload to s3
+aws s3 cp $FILE_NAME $S3_BUCKET --expires "$(date -I -d '30 days')" --quiet
+
+# delete file
+rm $FILE_NAME
